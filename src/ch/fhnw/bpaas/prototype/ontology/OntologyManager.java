c9aad12259e68c6ccc97a5192a9c686ec595075d
benlammel <benjamin.lammel@lammel.ch>
2016-06-30 15:51:48 +0200

    prepared for shipping v1.0

--- src/ch/fhnw/bpaas/prototype/ontology/OntologyManager.java
+++ src/ch/fhnw/bpaas/prototype/ontology/OntologyManager.java
@@ -37,7 +37,10 @@ public class OntologyManager {
 
 	private void readExistingModels() {
 		for(ONTOLOGY ontology : ONTOLOGY.values()){
-			rdfModel.read(ontology.getRemoteURL(),	ontology.getFormat());
+			System.out.println("loading " +ontology);
+			RDFDataMgr.read(rdfModel, this.getClass().getClassLoader().getResourceAsStream(ontology.getRemoteURL()), Lang.TTL);
+//			rdfModel.read(ontology.getRemoteURL(),	ontology.getFormat());
+			System.out.println(ontology +" loaded.");
 		}
 		printToFile(rdfModel, GlobalVariable.INITIALMODEL);
 	}
